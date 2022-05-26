b4f9e48e5b0aa7fd2aae1718e51da6343da0d90b
ywfumav <101041538+ywfumav@users.noreply.github.com>
2022-05-26 15:56:29 +0800

    Update timepi.sh

--- timepi.sh
+++ timepi.sh
@@ -5,7 +5,7 @@
 # This script will retrieve and log both the CPU and GPU temperature on the Raspberry Pi 4.
 
 cpu=$(</sys/class/thermal/thermal_zone0/temp)
-echo "Pi Temperature of $(hostname) @ $(date)" >> /home/ywfu/pitemp/templog.txt
-echo "CPU Temperature is => $((cpu/1000)) C" >> /home/ywfu/pitemp/templog.txt
-echo "GPU Temperature is => $(/opt/vc/bin/vcgencmd measure_temp)" >> /home/ywfu/pitemp/templog.txt
-echo "----------------------------" >> /home/ywfu/pitemp/templog.txt
+echo "Pi Temperature of $(hostname) @ $(date)" >> /home/<username>/pitemp/templog.txt
+echo "CPU Temperature is => $((cpu/1000)) C" >> /home/<username>/pitemp/templog.txt
+echo "GPU Temperature is => $(/opt/vc/bin/vcgencmd measure_temp)" >> /home/<username>/pitemp/templog.txt
+echo "----------------------------" >> /home/<username>/pitemp/templog.txt
